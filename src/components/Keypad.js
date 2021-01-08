1b73ffdda73e64373de22e96c029e4936987cb32
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2021-01-08 09:27:57 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,5 +1,8 @@
 // Code Keypad Component Here
+<<<<<<< HEAD
 import React from 'react';
+=======
+>>>>>>> eb2d590eb6d061cf86b644a37b2a6b6fd5aef2c7
 export default class Keypad extends React.Component {
 
   handleInput = () => console.log('Entering password...')
